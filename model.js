455701d5d0ccef5e1b057152c73e49a6fbc123b6
Ruchira Wageesha <ruchiraw@leapset.com>
2015-10-19 13:12:26 +0530

    updating

---
+++ model.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var permission = require('permission');
+
+var Schema = mongoose.Schema;
+
+var role = Schema({
+    name: String,
+    description: String,
+    has: {type: Object, default: {}},
+    allowed: {type: Object, default: {}}
+});
+
+role.methods.can = function (perm, action) {
+    return permission.has(this.has, perm.split(':'), action);
+};
+
+role.methods.permit = function (perm, actions, done) {
+    actions = actions instanceof Array ? actions : [actions];
+    permission.add(this.has, perm.split(':'), actions);
+    this.save(done);
+};
+
+role.virtual('id').get(function () {
+    return this._id;
+});
+
+module.exports = mongoose.model('Role', role);
