0d5fe88826ac2f4faa72f5350a5ac14355935f5c
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-04-17 00:51:54 +0530

    sso changes

--- model.js
+++ model.js
@@ -10,6 +10,14 @@ var role = Schema({
     allowed: {type: Object, default: {}}
 });
 
+role.set('toJSON', {
+    getters: true,
+    //virtuals: false,
+    transform: function (doc, ret, options) {
+        delete ret._id;
+    }
+});
+
 role.methods.can = function (perm, action) {
     return permission.has(this.has, perm.split(':'), action);
 };
